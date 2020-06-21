4251dd6e7e6df3498edb2d6333eb33214f64de38
CopoceanDacian <dacian.copocean@ferendia.com>
2020-06-22 01:59:32 +0300

    Table and create method

--- assets/js/components/TodoTable.js
+++ assets/js/components/TodoTable.js
@@ -1,14 +1,49 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react';
 import { TodoContext } from '../contexts/TodoContext';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Table, TableHead, TableRow, TableCell, TableBody, IconButton, TextField } from '@material-ui/core';
 
 export default function TodoTable() {
     const context = useContext(TodoContext);
+    const [addTodo, setAddTodo] = useState('');
 
     return (
-        <div>
-            {context.todos.map(todo => (
-                <div>{todo.name}</div>
-            ))}        
-        </div>
+        <form onSubmit={(event) => {context.createTodo(event, { name: addTodo })}}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Task</TableCell>
+                        <TableCell align="right">Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            <TextField value={addTodo} onChange={(event) => {setAddTodo(event.target.value)}} label="New Task" fullWidth={true}/>
+                        </TableCell>
+                        <TableCell align="right">
+                            <IconButton type="submit">
+                                <AddIcon />
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                    {context.todos.slice().reverse().map((todo, index) => (
+                        <TableRow key={'todo' + index}>
+                            <TableCell>{todo.name}</TableCell>
+                            <TableCell align="right">
+                                <IconButton>
+                                    <EditIcon />
+                                </IconButton>
+                                <IconButton>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </form>
     )
 }
